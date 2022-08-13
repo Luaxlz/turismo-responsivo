5e07c8ec71224d68b708b2c9e4679158905aa6a4
Lucas Angeli <jl.angeli@outlook.com>
2022-08-12 21:40:06 -0300

    adicionando animações

--- src/script.js
+++ src/script.js
@@ -1,9 +1,24 @@
 
-        window.onhashchange = function(e) {
-            const oldURL = e.oldURL.split('#')[1]
-            const newURL = e.newURL.split('#')[1]
-            const oldLink = document.querySelector(`.menu-main a[href="#${oldURL}"]`)
-            const newLink = document.querySelector(`.menu-main a[href="#${newURL}"]`)
-            oldLink && oldLink.classList.remove('selected')
-            newLink && newLink.classList.add('selected')
-        }
+window.onhashchange = function(e) {
+    const oldURL = e.oldURL.split('#')[1]
+    const newURL = e.newURL.split('#')[1]
+    const oldLink = document.querySelector(`.menu-main a[href="#${oldURL}"]`)
+    const newLink = document.querySelector(`.menu-main a[href="#${newURL}"]`)
+    oldLink && oldLink.classList.remove('selected')
+    newLink && newLink.classList.add('selected')
+}
+
+function msg() {
+    $("#mensagem").addClass('ver');
+    setTimeout(function() {$("#mensagem").removeClass('ver'); }, 3000);
+  }
+
+function msgError() {
+    $("#booking-error").addClass('ver');
+    setTimeout(function() {$("#booking-error").removeClass('ver'); }, 3000);
+}
+
+function subsOk() {
+    $("#newsletter-ok").addClass('ver');
+    setTimeout(function() {$("#newsletter-ok").removeClass('ver'); }, 3000);
+}
